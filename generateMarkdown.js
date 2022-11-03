7b02ca62f6816097da5c358617d79848b3150237
Fatemeh Niaziorimi <fatemehniaziorimi@fatemehs-mbp.mynetworksettings.com>
2022-11-03 18:35:53 -0400

    Added some new codes

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,18 +1,31 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {
+   if (data.license === "None") {
+    return;
+   } else {
+    return renderLicenseSection(data)
+   }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+## Description
+${data.description}
+
+${renderLicenseSection(data)}
 
 `;
 }
--- index.js
+++ index.js
@@ -1,13 +1,61 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const fs = require("fs")
+//const generateMarkdown = require("generateMarkdown.js")
+
+
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+`;
+}
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+  type: "input",
+  name: "title",
+  message: "What is your projects name?"
+},
+{
+  type: "input",
+  name: "description",
+  message: "Provide a short description explaining the what, why, and how of your project."
+},
+{
+  type: "input",
+  name: "installation",
+  message: "What are the steps required to install your project?"
+},
+{
+  type: "input",
+  name: "usage",
+  message: "Provide instructions and examples for use. Include screenshots as needed."
+},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, function(err) {
+    console.log(data),
+    err ? console.log(err) : console.log("README.md Successfully generated!")
+  })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then(function(data) {
+    writeToFile("README.md", generateMarkdown(data));
+    console.log(data)
+  })
+}
 
 // Function call to initialize app
 init();
